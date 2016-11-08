7e3bad0721131d62ad35815c0c216cda753a5792
Timofey Titovets <nefelim4ag@gmail.com>
2016-11-09 00:05:24 +0300

    Store leagueoflegends.desktop as a file
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ esac
 cd "$(dirname "$0")" || exit 1
 if [ "$PREFIX" == "/" ]; then
     if [ "$UID" != "0" ]; then
-        [ ! -f /usr/bin/sudo ] && ERRO "Run by root or install sudo!"
+        command -v sudo &> /dev/null && ERRO "Run by root or install sudo!"
         SUDO=sudo
     else
         unset SUDO
@@ -23,13 +23,4 @@ fi
 $SUDO install -Dm644 ./leagueoflegends.conf     "$PREFIX/etc/leagueoflegends.conf"
 $SUDO install -Dm755 ./leagueoflegends          "$PREFIX/usr/bin/leagueoflegends"
 $SUDO install -Dm644 ./leagueoflegends.png      "$PREFIX/usr/share/icons/hicolor/48x48/apps/leagueoflegends.png"
-
-$SUDO mkdir -p "$PREFIX/usr/share/applications/"
-{
-    echo "[Desktop Entry]"
-    echo Name=League Of Legends
-    echo Comment=League Of Legends Game
-    echo Type=Application
-    echo Exec=leagueoflegends
-    echo Icon=leagueoflegends
-} | $SUDO tee "$PREFIX/usr/share/applications/leagueoflegends.desktop"
+$SUDO install -Dm644 ./leagueoflegends.desktop  "$PREFIX/usr/share/applications/leagueoflegends.desktop"

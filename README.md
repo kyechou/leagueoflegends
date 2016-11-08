847f6807ebe4a78136e5f1059a536fe8aba5f92f
Timofey Titovets <nefelim4ag@gmail.com>
2016-11-08 03:56:17 +0300

    First release
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # League-Of-Legends
 Linux: League Of Legends www.leagueoflegends.com install/run wrapper
+
+Tested: Archlinux 08.11.2016 wine-staging 1.9.21
---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+################################################################################
+# echo wrappers
+INFO(){ echo -n "INFO: "; echo "$@" ;}
+WARN(){ echo -n "WARN: "; echo "$@" ;}
+ERRO(){ echo -n "ERRO: "; echo -n "$@" ; echo " Abort!"; exit 1;}
+
+PREFIX="/"
+case $1 in
+    PREFIX=*) PREFIX="${1//PREFIX=/}";;
+esac
+
+cd "$(dirname $0)" || exit 1
+if [ "$PREFIX" == "/" ]; then
+    if [ "$UID" != "0" ]; then
+        [ ! -f /usr/bin/sudo ] && ERRO "Run by root or install sudo!"
+        SUDO=sudo
+    else
+        unset SUDO
+    fi
+fi
+
+$SUDO install -Dm644 ./leagueoflegends.conf     $PREFIX/etc/leagueoflegends.conf
+$SUDO install -Dm755 ./leagueoflegends          $PREFIX/usr/bin/leagueoflegends
+$SUDO install -Dm644 ./leagueoflegends.png      $PREFIX/usr/share/icons/hicolor/48x48/apps/leagueoflegends.png
+
+$SUDO mkdir -p $PREFIX/usr/share/applications/
+{
+    echo "[Desktop Entry]"
+    echo Encoding=UTF-8
+    echo Name=League Of Legeds
+    echo Comment=League Of Legeds Game
+    echo Type=Application
+    echo Exec=$PREFIX/usr/bin/leagueoflegends
+    echo Icon=$PREFIX/usr/share/icons/hicolor/48x48/apps/leagueoflegends.png
+    echo StartupWMClass=LoLLauncher.exe
+} | $SUDO tee /usr/share/applications/leagueoflegends.desktop

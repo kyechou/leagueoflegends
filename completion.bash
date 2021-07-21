c4cb6804b0812baa7089ed6657ee018a09bd7895
absurd <absurd@ryzen.lan>
2021-07-21 13:47:18 +0200

    Added an automatic setup for the DXVK dlls (It doubles the framerate)

--- completion.bash
+++ completion.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-complete -W "start install uninstall reinstall cleanup_logs kill run" leagueoflegends
+complete -W "start install uninstall reinstall add-dxvk del-dxvk cleanup_logs kill run" leagueoflegends

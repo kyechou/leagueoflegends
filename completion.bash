91ac15e4586f4a180337a9a03fa98f1c3a4d4d87
kyechou <kuanyenchou@gmail.com>
2022-02-12 13:51:42 -0600

    Update completion.bash for rm-dxvk-cache

--- completion.bash
+++ completion.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-complete -W "start install uninstall reinstall start-garena install-garena uninstall-garena reinstall-garena add-dxvk del-dxvk cleanup-logs kill kill-garena run run-garena" leagueoflegends
+complete -W "start install uninstall reinstall start-garena install-garena uninstall-garena reinstall-garena add-dxvk del-dxvk rm-dxvk-cache cleanup-logs kill kill-garena run run-garena" leagueoflegends

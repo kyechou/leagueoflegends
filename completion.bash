5ce7de665b0bafe15c0369ff71edbfee60d10e43
kyechou <kuanyenchou@gmail.com>
2022-08-07 21:59:31 -0500

    Update completion script

--- completion.bash
+++ completion.bash
@@ -1,3 +1,22 @@
 #!/usr/bin/env bash
 
-complete -W "start install uninstall reinstall start-garena install-garena uninstall-garena reinstall-garena add-dxvk del-dxvk rm-dxvk-cache cleanup-logs kill kill-garena run run-garena" leagueoflegends
+COMMANDS=(
+    start
+    install
+    uninstall
+    reinstall
+    start-garena
+    install-garena
+    uninstall-garena
+    reinstall-garena
+    add-dxvk
+    del-dxvk
+    rm-dxvk-cache
+    cleanup-logs
+    kill
+    kill-garena
+    run
+    run-garena
+)
+
+complete -W "${COMMANDS[*]}" leagueoflegends
